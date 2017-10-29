0c7fca1041a0d0eb960782d505064b6981ec486e
MarvNet <marvin.kai@gmx.de>
2017-10-29 11:51:32 +0100

    Added GestureTest

---
+++ src/GestureTest1.js
@@ -0,0 +1,29 @@
+input.onGesture(Gesture.TiltLeft, () => {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltRight, () => {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.Shake, () => {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.setLedColor(Colors.Red)
+    music.playTone(Note.C, music.beat(BeatFraction.Whole))
+})
