aeac5fa25346247d0d934e0fd213c097934c3bd0
MarvNet <marvin.kai@gmx.de>
2017-10-30 15:05:28 +0100

    Added button exercise

---
+++ cpp_env/1/source/CalliopeButtonExercise.cpp
@@ -0,0 +1,16 @@
+#include "CalliopeSettings.h"
+#include "MicroBit.h"
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+
+
+#ifdef CALLIOPE_BUTTON_EXERCISE
+
+MicroBit uBit;
+
+int main() {
+    uBit.init();
+    
+}
+
+#endif
